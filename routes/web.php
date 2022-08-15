9b3c9f98c8ebb1673d729340e40dca73b6819604
MFMercado <ar.mflmercado@gmail.com>
2022-08-15 08:55:19 +0800

    updated cart controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Cart;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,10 +18,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Product Controller
 Route::get('/products', 'App\Http\Controllers\ProductController@getProducts');
-
 Route::post('/products', 'App\Http\Controllers\ProductController@postProducts');
 
+// Cart Controller
+// Route::post('/cart/get', 'App\Http\Controllers\CartController@getCart');
 Route::get('/cart', 'App\Http\Controllers\CartController@getCart');
-
+// Route::get('/cart/{id}', 'App\Http\Controllers\CartController@getCart');
 Route::post('/cart', 'App\Http\Controllers\CartController@postCart');
+Route::delete('/cart', 'App\Http\Controllers\CartController@deleteCart');
+Route::patch('/cart', 'App\Http\Controllers\CartController@updateCart');
+
+
+// Route::get('cart/{cart_id}', function ($cart_id) {
+//     $Carts = Route::get('/cart', 'App\Http\Controllers\CartController@getCart');
+//     return Carts.$cart_id;
+// });
+
+// Route::get('/cart/{id}', 'App\Http\Controllers\CartController@getCart');
